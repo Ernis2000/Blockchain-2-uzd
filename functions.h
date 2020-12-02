f35df5d6980fda83e1bd0784e64a757df200656d
Ernis2000 <56826586+Ernis2000@users.noreply.github.com>
2020-12-02 15:41:47 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -8,11 +8,12 @@
 #include <string>
 #include <random>
 #include <ctime>
+#include <bitcoin/bitcoin.hpp>
 
 const int USER_AMOUNT = 1000;
 const int TRANSACTION_AMOUNT = 10000;
 
 void generateUsers(std::vector<user>& users);
 void generateTransactions(std::vector<transaction>& Transactions, const std::vector<user>& users);
-std::string MerkleTree(std::vector<std::string>& transactions);
+bc::hash_digest create_merkle(bc::hash_list& merkle);
 void mining(Blockchain& block_chain, std::vector<Block>& Bblock, std::vector<transaction>& TransactionsToExecute);
